af1c3447e6d56676307685ba463439eb592ff098
Laurian <prevellaurian@gmail.com>
2022-01-28 15:46:37 +0100

    first commit

---
+++ monticketing/ticketingprocedural/Vue/vueAccueil.php
@@ -0,0 +1,20 @@
+<?php $titre = 'Mon ticketing procedural'; ?>
+
+<?php ob_start(); ?>
+<?php foreach ($billets as $billet): ?>
+    <article>
+        <header>
+            <a href="<?= "index.php?action=billet&id=" . $billet['id'] ?>">
+                <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+            </a>
+            <time><?= $billet['date'] ?></time>
+        </header>
+        <p><?= $billet['contenu'] ?></p>
+		        <p><?= $billet['etlib'] ?></p>
+
+    </article>
+    <hr />
+<?php endforeach; ?>
+<?php $contenu = ob_get_clean(); ?>
+
+<?php require 'gabarit.php'; ?>
